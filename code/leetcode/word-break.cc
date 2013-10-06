3acc6c524145949d851153a8000dc9a4cb5d8a1a
dirtysalt <dirtysalt1987@gmail.com>
2013-10-06 10:34:59 +0800

    [gc]commit

--- code/leetcode/word-break.cc
+++ code/leetcode/word-break.cc
@@ -5,7 +5,8 @@ using namespace std;
 
 #define unordered_set set
 
-// or use DP.
+// seems trie is a bad way since it has backtrace.
+#if 0 
 class Trie {
  public:
   char ch;
@@ -95,12 +96,11 @@ class Solution {
   
   bool wordBreak(string s, unordered_set<string> &dict) {
     // Note: The Solution object is instantiated only once and is reused by each test case.
-
+    
     // aaaa....aaaab
     // the dict contains [a,aa,aaa,...,aaaaaaaaaa];
     // seems it's a special case.    
     if(dict.size() == 10 && s[s.size()-1] == 'b') {
-      //printf("fuck");
       return false;
     }
     
@@ -122,14 +122,65 @@ class Solution {
   }
 };
 
+#endif
+
+class Solution {
+ public:
+  char* dp;
+  int N;
+  bool wordBreak(string s, unordered_set<string> &dict) {
+    // Note: The Solution object is instantiated only once and is reused by each test case.
+    const int n = s.size();
+    if(n == 0) {
+      return true;
+    }
+    N = n;
+    dp = new char[n*n];
+    memset(dp,0xff,sizeof(char)*n*n);
+    bool x = wb(s,0,n-1,dict);
+    delete[] dp;
+    return x;
+  }
+  int getIndex(int i,int j) {
+    return i*N+j;
+  }
+  bool wb(const string& s,int f,int e,unordered_set<string>& dict) {
+    if(f > e) {
+      return true;
+    }
+    int index = getIndex(f,e);
+    if(dp[index]!=-1) {
+      return dp[index] == 1;
+    }
+    string sub = s.substr(f,e-f+1);
+    bool res = false;
+    if(dict.find(sub)==dict.end()) {      
+      for(int i=f+1;i<=e;i++) {
+        // [f,i-1],[i,e]
+        bool b1 = wb(s,f,i-1,dict);
+        bool b2 = wb(s,i,e,dict);
+        if(b1 && b2) {
+          res = true;
+          break;
+        }
+      }
+    } else {
+      res = true;
+    }
+    dp[index] = res ? 1 : 0;
+    return res;
+  }
+};
+
+
 int main() {
   Solution s;
-  // {
-  //   set<string> dict;
-  //   dict.insert("leet");
-  //   dict.insert("code");
-  //   printf("%d\n",s.wordBreak("leetcode",dict));
-  // }
+  {
+    set<string> dict;
+    dict.insert("leet");
+    dict.insert("code");
+    printf("%d\n",s.wordBreak("leetcode",dict));
+  }
   {
     const char* x[] = {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa",NULL};
     set<string> dict;
@@ -138,9 +189,9 @@ int main() {
     }
     printf("%d\n",s.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",dict));    
   }
-  // {
-  //   set<string> dict;
-  //   printf("%d\n",s.wordBreak("",dict));
-  // }
+  {
+    set<string> dict;
+    printf("%d\n",s.wordBreak("",dict));
+  }
   return 0;
 }
