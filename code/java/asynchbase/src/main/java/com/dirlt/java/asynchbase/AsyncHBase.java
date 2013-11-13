2cfd5eb7f1ece24dd71d721cefbcec08983fd5fe
dirtysalt <dirtysalt1987@gmail.com>
2013-11-13 18:54:38 +0800

    [gc]commit

--- code/java/asynchbase/src/main/java/com/dirlt/java/asynchbase/AsyncHBase.java
+++ code/java/asynchbase/src/main/java/com/dirlt/java/asynchbase/AsyncHBase.java
@@ -39,6 +39,11 @@ public class AsyncHBase {
                     GetRequest get = new GetRequest(Bytes.toBytes(table), Bytes.toBytes("r1"));
                     get.family(cf).qualifier("key");
                     Deferred<ArrayList<KeyValue>> getFuture = client.get(get);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (Exception e) {
+
+                    }
                     getFuture.addCallback(new Callback<Object, ArrayList<KeyValue>>() {
                         // get over. try to validate it.
                         public Object call(ArrayList<KeyValue> kvs) {
@@ -48,36 +53,36 @@ public class AsyncHBase {
                             System.out.println("finish request1");
 
 
-                            // before call shutdown, we raise another request.
-                            {
-                                System.out.println("start request2");
-                                PutRequest put = new PutRequest(Bytes.toBytes(table),
-                                        Bytes.toBytes("r2"), Bytes.toBytes(cf), Bytes.toBytes("key"),
-                                        Bytes.toBytes("value"));
-                                Deferred<Object> putFuture = client.put(put);
-                                putFuture.addCallback(new Callback<Object, Object>() {
-                                    public Object call(Object obj) {
-                                        // put over. try to get it.
-                                        GetRequest get = new GetRequest(Bytes.toBytes(table), Bytes.toBytes("r2"));
-                                        get.family(cf);
-                                        //qualifier("key");
-                                        Deferred<ArrayList<KeyValue>> getFuture = client.get(get);
-                                        getFuture.addCallback(new Callback<Object, ArrayList<KeyValue>>() {
-                                            // get over. try to validate it.
-                                            public Object call(ArrayList<KeyValue> kvs) {
-                                                assert (kvs.size() == 1);
-                                                KeyValue kv = kvs.get(0);
-                                                assert (Bytes.toString(kv.value()).equals("value"));
-                                                System.out.println("finish request2");
-                                                System.out.println("called shutdown after request2");
-                                                client.shutdown();
-                                                return null;
-                                            }
-                                        });
-                                        return null;
-                                    }
-                                });
-                            }
+//                            // before call shutdown, we raise another request.
+//                            {
+//                                System.out.println("start request2");
+//                                PutRequest put = new PutRequest(Bytes.toBytes(table),
+//                                        Bytes.toBytes("r2"), Bytes.toBytes(cf), Bytes.toBytes("key"),
+//                                        Bytes.toBytes("value"));
+//                                Deferred<Object> putFuture = client.put(put);
+//                                putFuture.addCallback(new Callback<Object, Object>() {
+//                                    public Object call(Object obj) {
+//                                        // put over. try to get it.
+//                                        GetRequest get = new GetRequest(Bytes.toBytes(table), Bytes.toBytes("r2"));
+//                                        get.family(cf);
+//                                        //qualifier("key");
+//                                        Deferred<ArrayList<KeyValue>> getFuture = client.get(get);
+//                                        getFuture.addCallback(new Callback<Object, ArrayList<KeyValue>>() {
+//                                            // get over. try to validate it.
+//                                            public Object call(ArrayList<KeyValue> kvs) {
+//                                                assert (kvs.size() == 1);
+//                                                KeyValue kv = kvs.get(0);
+//                                                assert (Bytes.toString(kv.value()).equals("value"));
+//                                                System.out.println("finish request2");
+//                                                System.out.println("called shutdown after request2");
+//                                                client.shutdown();
+//                                                return null;
+//                                            }
+//                                        });
+//                                        return null;
+//                                    }
+//                                });
+//                            }
                             // doesn't matter because we already init a request.
                             System.out.println("called shutdown after request1");
                             client.shutdown();
